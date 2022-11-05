1fe3bae413f7de67cc2c7a7022fde4086273b632
kanniyappan01 <kanniyappan646@gmail.com>
2022-11-05 20:04:03 +0530

    if else practice

---
+++ js03 (if else)/js/script.js
@@ -0,0 +1,68 @@
+// if else
+
+/*
+var weather = "hot";
+
+if(weather === "hot"){
+	console.log("weather is hot!")
+}
+
+else{
+	console.log("wheather is cold!")
+}
+
+*/
+//
+
+/*
+var isRaing = false;
+var isCloudy = true;
+
+if(isRaing || isCloudy){
+	console.log("don't forget to take umberlaa.");
+}
+
+else{
+	console.log("weather is normal! enjoy your day");
+}
+*/
+
+// if hour is between 1am(0) to 12pm(12)  --. Good Mornig
+// else if hour is between 1pm(13) to 6pm(16)  -->good afternoon
+// else if hour is between 1pm(16) to 6pm(18)  -->good Evening
+// else balance hours are --> good Night	
+
+// var hour = 16;
+
+/*
+var hour = new Date();
+
+console.log(hour);
+console.log(hour.getHours());
+console.log(hour.getMinutes());
+
+if(hour >= 0 && hour <=12){
+	console.log("Good Moring")
+}
+
+else if(hour >= 12 && hour <= 15){
+	console.log("Good Afternoon")
+}
+
+else if(hour >= 16 && hour <= 18){
+	console.log("Good Eveing")
+}
+
+else{
+	console.log("Good Night")
+}
+
+*/
+/*
+var a = true;
+var b = true;
+
+var c = (a && b)? "yes":"no";
+
+console.log(c);
+*/
